cda41ad4e63dcce56a841f1aedfdd96dfd7a3059
Abbey Masters <abbeykmasters@gmail.com>
2019-07-31 12:11:27 -0700

    fixing travis

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
-import store from './data/store.js';
-import products from './data/products.js';
-import ProductSet from '../src/product-set.js';
-import { getRandomInt } from './utility-functions.js';
+// import store from './data/store.js';
+// import products from './data/products.js';
+// import ProductSet from '../src/product-set.js';
+// import { getRandomInt } from './utility-functions.js';
 
-const quizSection = document.getElementById('quiz-section');
-const resultsSection = document.getElementById('results-section');
-const productButtonsSection = document.getElementById('product-buttons');
-const submitButton = document.getElementById('submit-button');
+// const quizSection = document.getElementById('quiz-section');
+// const resultsSection = document.getElementById('results-section');
+// const productButtonsSection = document.getElementById('product-buttons');
+// const submitButton = document.getElementById('submit-button');
 
-let turns = 0;
+// let turns = 0;
 
-const allProducts = store.getProducts();
-const masterProductSet = new masterProductSet(allProducts);
+// const allProducts = store.getProducts();
+// const masterProductSet = new masterProductSet(allProducts);
 
 
--- src/product-set.js
+++ src/product-set.js
@@ -20,18 +20,18 @@ class ProductSet {
             }
             return;
         }
-    }
+    }}
 
-    // need new array of three images
-    // and a new list without the clicked image
-    // need to loop through three times to get three images
-    getThreeArray() {
-        let randomThree = [];
-    }
+//     // need new array of three images
+//     // and a new list without the clicked image
+//     // need to loop through three times to get three images
+//     getThreeArray() {
+//         let randomThree = [];
+//     }
 
-    hasProducts() {
-        return this.list.length > 0;
-    }
-}
+//     hasProducts() {
+//         return this.list.length > 0;
+//     }
+// }
 
 export default ProductSet;
