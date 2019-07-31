c4570bdb6a2f7e3cc3982b612c9b5d9b18b94c91
Abbey Masters <abbeykmasters@gmail.com>
2019-07-31 15:53:31 -0700

    buttons working

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import ProductSet from '../src/product-set.js';
 const buttonOne = document.getElementById('button-1');
 const buttonTwo = document.getElementById('button-2');
 const buttonThree = document.getElementById('button-3');
+const quizSection = document.getElementById('quiz-section');
+const resultsSection = document.getElementById('results-section');
 
 let turns = 0;
 
@@ -14,20 +16,61 @@ const allProducts = store.getProducts();
 const newProductSet = new ProductSet(allProducts);
 
 // create new array for images to go in when shown on page
-let displayed = [];
+let shownImage = [];
 
 // get three randomProducts to push into displayed empty array
 const randomOne = newProductSet.getRandomProduct();
 buttonOne.firstChild.src = randomOne.image;
-displayed.push(randomOne);
+shownImage.push(randomOne);
 
 const randomTwo = newProductSet.getRandomProduct();
 buttonTwo.firstChild.src = randomTwo.image;
-displayed.push(randomTwo);
+shownImage.push(randomTwo);
 
 const randomThree = newProductSet.getRandomProduct();
 buttonThree.firstChild.src = randomThree.image;
-displayed.push(randomThree);
+shownImage.push(randomThree);
 
 // need to make button run 
 
+const buttons = document.querySelectorAll('button');
+
+for(let i = 0; i < buttons.length; i++) {
+    const button = buttons[i];
+    button.addEventListener('click', handleUserChoice);
+}
+
+function handleUserChoice() {
+
+// need to take last shown images out of array and reset shownImage array
+    const removedSet = new ProductSet(allProducts);
+   
+    for(let i = 0; i < shownImage.length; i++) {
+        removedSet.removeById(shownImage[i].id);
+    }
+   
+    shownImage = [];
+
+// get three randomProducts to push into new array
+    const randomOne = removedSet.getRandomProduct();
+    buttonOne.firstChild.src = randomOne.image;
+    shownImage.push(randomOne);
+
+    const randomTwo = removedSet.getRandomProduct();
+    buttonTwo.firstChild.src = randomTwo.image;
+    shownImage.push(randomTwo);
+
+    const randomThree = removedSet.getRandomProduct();
+    buttonThree.firstChild.src = randomThree.image;
+    shownImage.push(randomThree);
+    
+// adds a turn
+    turns++;
+
+// checking turns
+    if(turns === 25) {
+        quizSection.classList.add('hidden');
+        resultsSection.classList.remove('hidden');
+    }
+    
+}
